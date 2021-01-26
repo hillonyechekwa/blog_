cedc42508b65ee88da1d6a2a7d4de6901e11f6d0
Hill Onyechekwa <Hillonyechekwa@gmail.com>
2021-01-26 05:18:10 +0100

    fixed: about component error

--- pages/about.js
+++ pages/about.js
@@ -1,19 +1,31 @@
 import { GraphQLClient } from "graphql-request"
 import Image from 'next/image'
 
-const About = ({ author }) => {
+const About = ({ authors }) => {
+
     return (
         <div>
-            <h1>{author.name}</h1>
-            <Image
-                src={author.avatar.url}
-                alt="author avatar"
-            />
-
-            <p>{author.bibliography}</p>
-            <div>{author.links.map(link => (
-                <p key={link}>{link}</p>
-            ))}</div>
+            {
+                authors.map(author => {
+                    return (
+                        <div key={author}>
+                            <h1>{author.name}</h1>
+                            <Image
+                                src={author.avatar.url}
+                                alt="author avatar"
+                                width={270}
+                                height={400}
+                            />
+                            <p>{author.bibliography}</p>
+                            <div>{author.links.map(link => {
+                                return (
+                                    <p key={link}>{link}</p>
+                                )
+                            })}</div>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
@@ -21,53 +33,30 @@ const About = ({ author }) => {
 const graphcms = new GraphQLClient(process.env.GRAPHCMS_API)
 
 
-export async function getStaticProps({ params }) {
-    const { author } = await graphcms.request(
+export async function getStaticProps() {
+    const { authors } = await graphcms.request(
         `
-            query MyQuery($id: ID) {
-                author(where: {id: $id}) {
+            query AboutPageQuery {
+                authors{
+                    id
                     name
                     bibliography
                     links
                     avatar {
-                    url(transformation: {image: {resize: {fit: scale, height: 100, width: 100}}})
+                    url
                     }
                 }
             }
-        `, {
-        id: params.id
-    }
+        `
     )
 
     return {
         props: {
-            author
+            authors
         }
     }
 }
 
-export async function getStaticPaths() {
-    const { authors } = await graphcms.request(
-        `
-        {
-            authors{
-                id
-                name
-            }
-        }
-        `
-    )
-
-    return {
-        paths: authors.map((author) => {
-            return {
-                params: {
-                    id: author.id
-                }
-            }
-        })
-    }
-}
 
 
 export default About
