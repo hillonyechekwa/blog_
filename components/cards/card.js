a60cc3590274cf2abd033e5454399032fd1a44b7
hill Onyechekwa <hillonyechekwa@gmail.com>
2021-02-13 00:53:50 +0100

    fixed index page card link

--- components/cards/card.js
+++ components/cards/card.js
@@ -1,27 +1,18 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Card = ({ items }) => {
+	const { title, slug, coverImage, author, excerpt } = items;
+	return (
+		<Link href={`/posts/${slug}`}>
+			<div>
+				<Image src={coverImage.url} alt="thumbnail" width={300} height={150} />
+				<h2>{title}</h2>
+				<p>{author.name}</p>
+				<p>{excerpt}</p>
+			</div>
+		</Link>
+	);
+};
 
-    const { title, slug, coverImage, author, excerpt } = items
-    return (
-        <Link href={`/post/${slug}`}>
-            <div>
-                <Image
-                    src={coverImage.url}
-                    alt="thumbnail"
-                    width={300}
-                    height={150}
-                />
-                <h2>{title}</h2>
-                <p>{author.name}</p>
-                <p>{excerpt}</p>
-            </div>
-        </Link>
-    )
-}
-
-
-export default Card
-
-
+export default Card;
