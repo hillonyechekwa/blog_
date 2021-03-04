e92c35e6ca00bff51e5aed614161e03aefb55819
hill Onyechekwa <hillonyechekwa@gmail.com>
2021-03-04 04:08:09 +0100

    created and styled 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Layout from '../components/layout'
+import Image from 'next/image'
 
 const NotFound = () => {
     return (
@@ -9,8 +11,16 @@ const NotFound = () => {
                 <title>NotFound</title>
             </Head>
             {/* TODO: beautify 404 page */}
-            The page you were looking for doesn't exist
-        </div>
+            <div className="notfoundwrapper">
+            <p>Hey there! Sorry you couldn't find the page your were looking for. click <Link href="/">here</Link> to go back to the home page</p>
+                <Image
+                    src="/images/404.png"
+                    alt="astronaut"
+                    width={500}
+                    height={300}
+                />
+            </div>
+         </div>
     )
 }
 
