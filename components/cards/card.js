5f85c4759ecf9d9a26be7f74aa0c95633c92a227
hill Onyechekwa <hillonyechekwa@gmail.com>
2021-04-01 16:03:30 +0100

    made card text responsive

--- components/cards/card.js
+++ components/cards/card.js
@@ -5,11 +5,11 @@ const Card = ({ items }) => {
 
 	return (
 		<Link className="cardlink" passHref href={`/posts/${slug}`}>
-		<svg id="card" viewBox="0 0 400 200">
+		<svg id="card" viewBox="0 0 1000 200">
 				<text id="card-text">
-					<tspan id="title" x="-300" y="100">{title}</tspan>
-					<tspan id="author" x="-300" y="150">{author.name}</tspan>
-					<tspan id="excerpt" x="-300" y="170">{excerpt}</tspan>
+					<tspan id="title" x="0" y="100">{title}</tspan>
+					<tspan id="author" x="0" y="150">{author.name}</tspan>
+					<tspan id="excerpt" x="0" y="170">{excerpt}</tspan>
 				</text>
 			</svg>
 			</Link>
