ca909d246e2fec01ddb8d509555006a47d5c4562
hill Onyechekwa <hillonyechekwa@gmail.com>
2021-04-25 17:55:58 +0100

    added navbar and onscroll interaction

--- components/layout.js
+++ components/layout.js
@@ -5,10 +5,8 @@ import Footer from './footer';
 const Layout = ({ children }) => {
 	return (
 		<div className="layout-wrapper">
-			<div className="layout-inner-wrapper">
 				{/* <Header /> */}
 				<Navigation />
-			</div>
 			<main className="layout-main-wrapper">{children}</main>
 			<Footer />
 		</div>
--- components/navigation.js
+++ components/navigation.js
@@ -1,14 +1,54 @@
 import Link from 'next/link'
+import {useState, useEffect} from 'react'
 
 const Navigation = () => {
+    const [scrolled, setScrolled] = useState(false);
+    console.log(scrolled)
+
+    useEffect(() => {
+
+        function handleScroll() {
+            const lastScrollY = window.scrollY;
+
+            if (lastScrollY > 50) {
+                setScrolled(true)
+                console.log(scrolled)
+            } else {
+                setScrolled(false)
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll)
+            
+    }, [])
+
+
+
     return (
-        <nav className="navigation">
+        <nav className={scrolled === true? 'navigation navscrolled': `navigation`}>
             <div className="Logo"></div>
             <ul className="navitems">
-                <li className="navitem"><Link href="/">Home</Link></li>
-                <li className="navitem"><Link href="/">Posts</Link></li>
-                <li className="navitem"><Link href="/">About</Link></li>
-                <li className="navitem"><Link href="/">buymeacoffee</Link></li>
+                <li className="navitem">
+                    <Link href="/">Home</Link>
+                    {/* <svg className="" width="">
+                        <path d="" pathLength="1"/>
+                    </svg> */}
+                </li>
+                <li className="navitem">
+                    <Link href="/">Posts</Link>
+                    {/* <svg className="" width="">
+                        <path d="" pathLength="1"/>
+                    </svg> */}
+                </li>
+                <li className="navitem">
+                    <Link href="/">About</Link>
+                    {/* <svg className="" width="">
+                        <path d="" pathLength="1"/>
+                    </svg> */}
+                </li>
+                <li className="navitem">
+                    <Link href="/">buymeacoffee</Link>
+                </li>
             </ul>
         </nav>
     )
